f87fe87f5c756689ffe636c0cd26cef6356c3090
fraugho <aghoanbeom@gmail.com>
2024-03-19 23:25:35 -0800

    Redis Credential Validation implementation and Main function refactoring

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,4 +1,4 @@
-use gloo_net::websocket::futures::WebSocket;
+use gloo_net::websocket::{futures::WebSocket, Message};
 use reqwasm::http;
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::Uuid;
@@ -9,11 +9,10 @@ use web_sys::{window, HtmlInputElement, Request, RequestInit, RequestMode, Respo
 use yew::events::SubmitEvent;
 use yew::prelude::*;
 
-
-
 #[function_component(HomePage)]
 pub fn home_page() -> Html {
-    let mut _ws = WebSocket::open("ws://0.0.0.0:8080").unwrap();
+    let mut ws = WebSocket::open("ws://0.0.0.0:8080").unwrap();
+
     let onclick = Callback::from(|_| {
         let document = window().unwrap().document().unwrap();
 
@@ -25,7 +24,7 @@ pub fn home_page() -> Html {
             <h1 style="text-align: center; margin: 10; padding: 0;">{ "BlackSignal" }</h1>
             <div>
                 <input type="text" id="chat-area" placeholder={"Write Something"} />
-                <button onclick={onclick}>{"Login"}</button>
+                <button type="message-submit" onclick={onclick}>{"Login"}</button>
             </div>
         </main>
     }
